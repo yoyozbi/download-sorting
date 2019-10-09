399be9be6955f7268b813747623f24761e9f7065
yoyozbi <yohan.zbinden@gmail.com>
2019-10-09 20:57:31 +0200

    begin the auto compress module

--- script.py
+++ script.py
@@ -2,8 +2,9 @@ import os
 import time
 import glob
 from myPythonModules import other
+from zipfile import ZipFile
 import json 
-
+import datetime
 properties = {}
 class ForJson:
     def createJson(self):
@@ -20,6 +21,7 @@ class ForJson:
         properties["archives"]["extension"] = ['rar', 'iso', 'zip', '7z']
 
         properties["other"] = properties["folder_to_track"] + "/autres/"
+        properties["old"] = properties["folder_to_track"] + "/vieux/"
         with open('./config.json','w') as f:
             json.dump(properties, f)
 
@@ -32,7 +34,31 @@ class ForJson:
         else:
             ForJson.createJson(self)
 
+class compress():
+    def findOldestFile(self):
+        files = compress.get_all_file_paths(self, properties["folder_to_track"])
+        biggest = 
+        for i in range(files):
 
+    def get_all_file_paths(self,directory):
+            # initializing empty file paths list
+        file_paths = []
+
+        # crawling through directory and subdirectories
+        for root, directories, files in os.walk(directory):
+            for filename in files:
+                # join the two strings in order to form the full filepath.
+                filepath = os.path.join(root, filename)
+                file_paths.append(filepath)
+
+        # returning all file paths
+        return file_paths
+    def compress(self, date):
+        files = compress.get_all_file_paths(self, properties["folder_to_track"])
+        actual_date = datetime.date.today()
+        with ZipFile(properties['old'] + actual_date.month + actual_date.year + ".zip") as zip:
+            for file in files:
+                zip.write(file)
 def renameFilesIfAsExtension(folder_source, folder_destination, extension):
     os.chdir(folder_source)
     allFiles = glob.glob('*.*')
@@ -48,7 +74,7 @@ def renameFilesIfAsExtension(folder_source, folder_destination, extension):
 
 def findFile():
     for i in properties:
-        if i != "username" and i != "folder_to_track" and i != "other":
+        if i != "username" and i != "folder_to_track" and i != "other" and i!= "old":
             print(i)
             for extension in properties.get(i)["extension"]:
                 renameFilesIfAsExtension(properties['folder_to_track'], properties[i]["folder_destination"], extension)    
